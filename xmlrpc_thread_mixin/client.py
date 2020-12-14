db836781c983010c07783a44dd9e3422802871d0
reggiepy <wangtongpy@foxmail.com>
2020-12-14 15:06:22 +0800

    [+] add instance method

--- xmlrpc_thread_mixin/client.py
+++ xmlrpc_thread_mixin/client.py
@@ -3,6 +3,7 @@
 from xmlrpc.client import ServerProxy
 
 if __name__ == '__main__':
-    server = ServerProxy("http://localhost:8888")  # 初始化服务器
+    server = ServerProxy("http://localhost:9999")  # 初始化服务器
     print(server.get_string("cloudox"))  # 调用函数1并传参
     print(server.add(8, 8))  # 调用函数2并传参
+    print(server.print("fasfd"))  # 调用函数2并传参
--- xmlrpc_thread_mixin/server.py
+++ xmlrpc_thread_mixin/server.py
@@ -3,8 +3,7 @@ from xmlrpc.server import SimpleXMLRPCServer
 from socketserver import ThreadingMixIn
 
 
-class ThreadXMLRPCServer(ThreadingMixIn, SimpleXMLRPCServer):
-    pass
+class ThreadXMLRPCServer(ThreadingMixIn, SimpleXMLRPCServer): pass
 
 
 # 调用函数1
@@ -17,9 +16,14 @@ def add(x, y):
     return x + y
 
 
+class T():
+    def print(self, string):
+        print(string)
+
 if __name__ == '__main__':
-    server = ThreadXMLRPCServer(('localhost', 8888))  # 初始化
+    server = ThreadXMLRPCServer(('localhost', 9999), allow_none=True)  # 初始化
     server.register_function(respon_string, "get_string")  # 注册函数1
     server.register_function(add, 'add')  # 注册函数2
+    server.register_instance(T(), 'T')  # 注册函数2
     print("Listening for Client")
     server.serve_forever()  # 保持等待调用状态
